846603f17ae1c2fbea8921b9b8cb662cfa61c006
CristianAGR <cgr72677@udelasalle.edu.mx>
2022-12-02 20:25:50 -0600

    Se agrego la funcion para calcular los promedios requeridos y se pasan a la siguiente actividad

--- app/src/main/java/com/example/finalandroid/RegisterActivity.kt
+++ app/src/main/java/com/example/finalandroid/RegisterActivity.kt
@@ -17,9 +17,24 @@ class RegisterActivity : AppCompatActivity() {
     fun logica() {
         btnCalcular.setOnClickListener {
             val intent = Intent(this,RequiredGradesActivity::class.java)
-            //intent.putExtra("cal6", txtMonto.text.toString())
-            //intent.putExtra("cal10", txtMonto.text.toString())
+            val parcial1 = (txtCal1.text.toString().toDouble())*.2
+            val parcial2 = (txtCal2.text.toString().toDouble())*.2
+            var calificacion6 = calcular(parcial1,parcial2).get(0)
+            var calificacion10 = calcular(parcial1,parcial2).get(1)
+            intent.putExtra("cal6", calificacion6)
+            intent.putExtra("cal10", calificacion10)
             startActivity(intent)
         }
+    }
+
+    // calcula cuanta calificacion se necesita para pasar la materia con 6 y con 10
+    private fun calcular(parcial1: Double, parcial2: Double): ArrayList<Double> {
+        val promedio= parcial1 + parcial2
+        var calificacion6 = (6 - promedio)/.6
+        var calificacion10 = (10 - promedio)/.6
+        var calificaciones = ArrayList<Double>()
+        calificaciones.add(calificacion6)
+        calificaciones.add(calificacion10)
+        return calificaciones
     }
 }
--- app/src/main/java/com/example/finalandroid/RequiredGradesActivity.kt
+++ app/src/main/java/com/example/finalandroid/RequiredGradesActivity.kt
@@ -3,6 +3,7 @@ package com.example.finalandroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.activity_required_grades.*
@@ -15,6 +16,22 @@ class RequiredGradesActivity : AppCompatActivity() {
     }
 
     fun logica() {
+        // recibe valores de MainActivity
+        val bundle = intent.extras;
+        val cal6 = bundle?.getDouble("cal6")
+        val cal10 = bundle?.getDouble("cal10")
+        val txtCali6 = findViewById<TextView>(R.id.txtCal6)
+        val txtCali10 = findViewById<TextView>(R.id.txtCal10)
+        txtCali6.text = cal6.toString()
+        txtCali10.text = cal10.toString()
+        if(txtCali10.text.toString().toDouble() <= 10.0) {
+            lblResultado.text = "Aún se puede"
+        } else {
+            lblResultado.text = "Ya no se logro"
+        }
+
+
+
         btnGuardar.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             //intent.putExtra("cal6", txtMonto.text.toString())
